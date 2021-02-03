6d1856a972ac511d1af4ebe17920b73fd188ff90
Epod <16759224+TIEpod@users.noreply.github.com>
2021-02-03 23:17:59 +0100

    t
    Scheint zu funktionieren

--- Konvergenz des Newtonverfahrens/NewtonverfahrenG96.py
+++ Konvergenz des Newtonverfahrens/NewtonverfahrenG96.py
@@ -4,11 +4,11 @@ def main():
     '''Es müssen leider die Fenster geschlossen werden damit das nächste kommt.
     Das einiege Sachen englisch und andere deutsch benannt sind hat keinen Hintergrund.
     Die Laufzeit kann sehr lange dauern.'''
-    exercise2()
-    exercise3()
-    aufgabe4()
-    aufgabe5()
-    aufgabe6()
-    exercise7()
+    pa4.exercise2()
+    pa4.exercise3()
+    pa4.aufgabe4()
+    pa4.aufgabe5()
+    pa4.aufgabe6()
+    pa4.exercise7()
 
 main()
